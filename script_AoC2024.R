d09e089103e056bc61d158d19d2c45abc6328dd3
Sophia Crouch <111587883+sbryson-limno@users.noreply.github.com>
2024-12-02 09:59:31 -0500

    Day 1, Puzzle 1 Solution

--- script_AoC2024.R
+++ script_AoC2024.R
@@ -31,19 +31,19 @@ library(readxl)
 # PUZZLE 1
   # There's just one problem: by holding the two lists up side by side (your puzzle input), it quickly becomes clear that the lists aren't very similar. Maybe you can help The Historians reconcile their lists?
   
-  list1 <- c(3, 4, 2, 1, 3, 3)
-  list2 <- c(4, 3, 5, 3, 9, 3)
-  
+  lists <- read.delim(paste0(input_dir, "D1_P1.txt"),
+                      sep = " ", #not parsing as tabs? 
+                      header = FALSE) %>% 
+           select(list1 = V1, list2 = V4)
   
   # Maybe the lists are only off by a small amount! 
   # To find out, pair up the numbers and measure how far apart they are. 
   # Pair up the smallest number in the left list with the smallest number in the right list, then the second-smallest left number with the second-smallest right number, and so on.
   
-  list1 <- list1 %>% sort()
-  list2 <- list2 %>% sort()
+  reconcile_df <- lists %>% mutate(list1 = sort(list1), 
+                                   list2 = sort(list2))
 
-  reconcile_df <- as.data.frame(cbind(list1, list2))
-
+  
   #Within each pair, figure out how far apart the two numbers are; you'll need to add up all of those distances. 
   #For example, if you pair up a 3 from the left list with a 7 from the right list, the distance apart is 4; if you pair up a 9 with a 3, the distance apart is 6.
 
@@ -53,5 +53,8 @@ library(readxl)
   # In the example above, this is 2 + 1 + 0 + 1 + 2 + 5, a total distance of 11!
   
   total_distance <- sum(reconcile_df$distance)
-
+  
+  total_distance
+  
+  
 ### Day 2 ######################################################################
