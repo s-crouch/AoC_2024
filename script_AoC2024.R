314729c196d3152c968451355415b5b5f18a8591
Sophia Crouch <111587883+sbryson-limno@users.noreply.github.com>
2024-12-05 12:33:09 -0500

    Day 3, Example 1
    Solved & simplified thanks to https://stackoverflow.com/questions/4271553/how-do-i-write-a-regular-expression-to-match-any-three-digit-number-value

--- script_AoC2024.R
+++ script_AoC2024.R
@@ -429,23 +429,34 @@ library(readxl)
   #specify range/number of wildards in regex?? 
   #   str_extract_all(corr_ex, "mul\\(.,.\\)") #https://stackoverflow.com/questions/5633533/regular-expression-for-matching-parentheses
 
-  str_extract_all(corrupt_data, regex("(?<=:)>.*"))
-  string = c("G1:E001", "G2:E002", "G3:E003")
-  regmatches(corrupt_data, regexpr('mul\\([0-999],[0-999]\\)', corrupt_data)) #https://stackoverflow.com/questions/17215789/extract-a-substring-according-to-a-pattern
+  # str_extract_all(corrupt_data, regex("(?<=:)>.*"))
+  # string = c("G1:E001", "G2:E002", "G3:E003")
+  # regmatches(corrupt_data, regexpr('mul\\([0-999],[0-999]\\)', corrupt_data)) #https://stackoverflow.com/questions/17215789/extract-a-substring-according-to-a-pattern
   
   
-  str_extract_all(corrupt_data, regex('mul\\([0-999],[0-999]\\)')) #misses 11,8? 
-  #Brute force because I can't figure out how to constrain wildcard length. Can rework. 
-  mul_statements <- c(unlist(str_extract_all(corr_ex, "mul\\(.,.\\)")), 
-                      unlist(str_extract_all(corr_ex, "mul\\(.,..\\)")),
-                      unlist(str_extract_all(corr_ex, "mul\\(.,...\\)")),
-                      unlist(str_extract_all(corr_ex, "mul\\(..,.\\)")),
-                      unlist(str_extract_all(corr_ex, "mul\\(..,..\\)")),
-                      unlist(str_extract_all(corr_ex, "mul\\(..,...\\)")),
-                      unlist(str_extract_all(corr_ex, "mul\\(...,.\\)")),
-                      unlist(str_extract_all(corr_ex, "mul\\(...,..\\)")),
-                      unlist(str_extract_all(corr_ex, "mul\\(...,...\\)")))
-  #duplicase 8,5
+  
+  # str_extract_all(corrupt_data, regex('mul\\([0-999],[0-999]\\)')) #misses 11,8? https://stackoverflow.com/questions/3148240/why-doesnt-01-12-range-work-as-expected
+  # 
+  # str_extract_all(corrupt_data, regex('mul\\([1-9][0-9],[1-9]\\)')) #catches only 11,8? https://stackoverflow.com/questions/3148240/why-doesnt-01-12-range-work-as-expected
+  # 
+  # str_extract_all(corrupt_data, regex('mul\\(\d{1},\d{1}\\)')) #misses 11,8? https://stackoverflow.com/questions/3148240/why-doesnt-01-12-range-work-as-expected
+  # 
+  # str_extract_all(corrupt_data, regex('\\d{1,3}')) #misses 11,8? https://stackoverflow.com/questions/3148240/why-doesnt-01-12-range-work-as-expected
+  
+  mul_statements <- unlist(str_extract_all(corrupt_data, regex('mul\\(\\d{1,3},\\d{1,3}\\)'))) #https://stackoverflow.com/questions/4271553/how-do-i-write-a-regular-expression-to-match-any-three-digit-number-value
+  
+
+  # #Brute force because I can't figure out how to constrain wildcard length. Can rework. 
+  # mul_statements <- c(unlist(str_extract_all(corr_ex, "mul\\(.,.\\)")), 
+  #                     unlist(str_extract_all(corr_ex, "mul\\(.,..\\)")),
+  #                     unlist(str_extract_all(corr_ex, "mul\\(.,...\\)")),
+  #                     unlist(str_extract_all(corr_ex, "mul\\(..,.\\)")),
+  #                     unlist(str_extract_all(corr_ex, "mul\\(..,..\\)")),
+  #                     unlist(str_extract_all(corr_ex, "mul\\(..,...\\)")),
+  #                     unlist(str_extract_all(corr_ex, "mul\\(...,.\\)")),
+  #                     unlist(str_extract_all(corr_ex, "mul\\(...,..\\)")),
+  #                     unlist(str_extract_all(corr_ex, "mul\\(...,...\\)")))
+  
   mul_statements
   
   num <- mul_statements %>% 
