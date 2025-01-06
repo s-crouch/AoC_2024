0f565cb55501655c911bbc17fded7ade79801ae3
Sophia Crouch <111587883+sbryson-limno@users.noreply.github.com>
2025-01-06 11:29:45 -0500

    Day 3, Puzzle 1 - Example, working

--- script_AoC2024.R
+++ script_AoC2024.R
@@ -523,3 +523,85 @@ library(readxl)
   
   prod_sum <- sum(num_pairs_df$prod) 
   prod_sum
+
+  
+### Day 4 ######################################################################
+  
+# PUZZLE 1 ---------------------------------------------------------------------
+  
+  # She'd like to know if you could help her with her word search (your puzzle input). She only has to find one word: XMAS.
+  # This word search allows words to be horizontal, vertical, diagonal, written backwards, or even overlapping other words. 
+  # It's a little unusual, though, as you don't merely need to find one instance of XMAS - you need to find all of them. 
+  # Here are a few ways XMAS might appear, where irrelevant characters have been replaced with .:
+  
+    # ..X...
+    # .SAMX.
+    # .A..A.
+    # XMAS.S
+    # .X....
+  
+#   The actual word search will be full of letters instead. For example:
+
+  #   MMMSXXMASM
+  #   MSAMXMSMSA
+  #   AMXSXMAAMM
+  #   MSAMASMSMX
+  #   XMASAMXAMM
+  #   XXAMMXXAMA
+  #   SMSMSASXSS
+  #   SAXAMASAAA
+  #   MAMMMXMMMM
+  #   MXMXAXMASX
+  
+#   In this word search, XMAS occurs a total of 18 times; here's the same word search again, but where letters not involved in any XMAS have been replaced with .:
+  
+  # ....XXMAS.
+  # .SAMXMS...
+  # ...S..A...
+  # ..A.A.MS.X
+  # XMASAMX.MM
+  # X.....XA.A
+  # S.S.S.S.SS
+  # .A.A.A.A.A
+  # ..M.M.M.MM
+  # .X.X.XMASX
+  
+  input_str <- "MMMSXXMASM
+               MSAMXMSMSA
+               AMXSXMAAMM
+               MSAMASMSMX
+               XMASAMXAMM
+               XXAMMXXAMA
+               SMSMSASXSS
+               SAXAMASAAA
+               MAMMMXMMMM
+               MXMXAXMASX"
+  
+  # test_str <- "....XXMAS.
+  #              .SAMXMS...
+  #              ...S..A...
+  #              ..A.A.MS.X
+  #              XMASAMX.MM
+  #              X.....XA.A
+  #              S.S.S.S.SS
+  #              .A.A.A.A.A
+  #              ..M.M.M.MM
+  #              .X.X.XMASX"
+  
+  input_str_split <- strsplit(input_str, "\n") %>% 
+                    unlist() %>% 
+                    str_replace_all(" ", "") #drop whitespace
+  
+  #Create a matrix with 1 letter in each row/column index 
+  search_matrix <- as.data.frame(matrix(nrow = 0, ncol = str_length(input_str_split[1])))
+  
+  for (i in 1:length(input_str_split)){
+    row <- str_split_1(input_str_split[i],"")
+    search_matrix <- rbind(search_matrix, row)
+    # print(row)
+  }
+  
+  search_matrix
+  
+
+  
